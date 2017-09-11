28b63d8775d252d1dfd21c8c28cd727aced95643
Petr Pícha <ppicha@ntis.zcu.cz>
2017-09-11 11:38:40 +0200

    Tisk CSV pro Codeface

---
+++ src/main/java/cz/zcu/kiv/spade/output/CodefaceBean.java
@@ -0,0 +1,191 @@
+package cz.zcu.kiv.spade.output;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class CodefaceBean {
+
+    private String externalId;
+    private String url;
+    private String issueType;
+    private String title;
+    private String description;
+    private String creationDate;
+    private String createdBy;
+    private String priority;
+    private String severity;
+    private String version;
+    private String startDate;
+    private String dueDate;
+    private String assignedTo;
+    private String estimateTime;
+    private String spentTime;
+    private String progress;
+    private String status;
+    private String resolution;
+    private String category;
+    private String commits;
+
+    public CodefaceBean() {
+    }
+
+    public String getExternalId() {
+        return externalId;
+    }
+
+    public void setExternalId(String externalId) {
+        this.externalId = externalId;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getIssueType() {
+        return issueType;
+    }
+
+    public void setIssueType(String issueType) {
+        this.issueType = issueType;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getSeverity() {
+        return severity;
+    }
+
+    public void setSeverity(String severity) {
+        this.severity = severity;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getAssignedTo() {
+        return assignedTo;
+    }
+
+    public void setAssignedTo(String assignedTo) {
+        this.assignedTo = assignedTo;
+    }
+
+    public String getEstimateTime() {
+        return estimateTime;
+    }
+
+    public void setEstimateTime(String estimateTime) {
+        this.estimateTime = estimateTime;
+    }
+
+    public String getSpentTime() {
+        return spentTime;
+    }
+
+    public void setSpentTime(String spentTime) {
+        this.spentTime = spentTime;
+    }
+
+    public String getProgress() {
+        return progress;
+    }
+
+    public void setProgress(String progress) {
+        this.progress = progress;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getResolution() {
+        return resolution;
+    }
+
+    public void setResolution(String resolution) {
+        this.resolution = resolution;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getCommits() {
+        return commits;
+    }
+
+    public void setCommits(String commits) {
+        this.commits = commits;
+    }
+}
